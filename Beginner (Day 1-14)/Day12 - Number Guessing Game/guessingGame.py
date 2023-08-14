a6c41126aab143d79712b2ef2c9e227247639554
Xian Cheng <xianhuicheng01@gmail.com>
2023-08-14 21:46:42 +0800

    Add files for day 12

---
+++ Beginner (Day 1-14)/Day12 - Number Guessing Game/guessingGame.py
@@ -0,0 +1,28 @@
+import random
+EASY = 10
+HARD = 5
+guess = 0
+
+def randomNum():
+  choice = int(input("\nPick the range you would like to guess from:\n\t[1] 1 to 10\n\t[2] 1 to 100\nEnter Choice: "))
+  range = 10 if choice == 1 else 100
+  return random.randint(1,range)
+
+print("\n\t\033[36mWELCOME TO NUMBER GUESSING GAME!\033[0m")
+difficulty = int(input("Choose level of difficulty:\n\t[1] Easy\n\t[2] Hard\nEnter Choice: "))
+limit = EASY if difficulty == 1 else HARD
+tries = limit
+answer = randomNum()
+while answer != guess and tries > 0:
+  print(f"\n-- You have {tries} turns left --")
+  guess = int(input("Guess the mystery number: "))
+  tries -= 1
+  if guess < answer:
+    print ("\033[35mThat's too low!\033[0m")
+  elif guess > answer:
+    print ("\033[35mThat's too high!\033[0m")
+
+if guess == answer:
+  print (f"\n\033[32mYOU WON! It took you {tries} tries to guess {answer}\033[0m")
+else:
+   print (f"\n\033[31mYOU LOSE! The number was {answer}\033[0m")
--- README.md
+++ README.md
@@ -13,4 +13,5 @@ Compilation of source code for the projects made while taking the Udemy course: 
 - [Day 7: ](<https://github.com/xialuna/100-Days-of-Python/tree/main/Beginner%20(Day%201-14)/Day%207%20-%20Hangman%20Game>) Hangman Game
 - [Day 8: ](<https://github.com/xialuna/100-Days-of-Python/tree/main/Beginner%20(Day%201-14)/Day%208%20-%20Caesar%20Cipher>) Caesar Cipher
 - [Day 9: ](<https://github.com/xialuna/100-Days-of-Python/tree/main/Beginner%20(Day%201-14)/Day%209%20-%20Secret%20Auction%20Program>) Secret Auction Program
-- (updated every 5-10 days)
+- [Day 10: ](<https://github.com/xialuna/100-Days-of-Python/tree/main/Beginner%20(Day%201-14)/Day10%20-%20Calculator>) Calculator
+- [Day 11: ](<https://github.com/xialuna/100-Days-of-Python/tree/main/Beginner%20(Day%201-14)/Day11%20-%20Blackjack%20Capstone%20Project>) Blackjack Capstone Project
